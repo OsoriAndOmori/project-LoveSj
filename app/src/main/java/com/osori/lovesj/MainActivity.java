2776906e05accefe103e6fad0cad7ac0d9e6f496
OsoriAndOmori <skywhite15@korea.ac.kr>
2019-08-20 18:17:45 +0900

    add textView Test

--- app/src/main/java/com/osori/lovesj/MainActivity.java
+++ app/src/main/java/com/osori/lovesj/MainActivity.java
@@ -1,7 +1,11 @@
 package com.osori.lovesj;
 
+import java.time.LocalDate;
+
+import com.osori.lovesj.dday.DayCalculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TextView weddingDday = findViewById(R.id.wedding_dday);
+		weddingDday.setText("Since 2019. 4. 20 - " + DayCalculator.countDaysIncludeStandardFrom(LocalDate.of(2019, 4, 20), LocalDate.now()) + " days");
+
+		TextView loveDday = findViewById(R.id.love_dday);
+		loveDday .setText("Since 2014. 4. 19 - " + DayCalculator.countDaysIncludeStandardFrom(LocalDate.of(2014, 4, 19), LocalDate.now()) + " days");
 	}
 }
