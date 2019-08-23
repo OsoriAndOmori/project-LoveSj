535572e8cc59b5002fe88d03b13c114dac51e878
OsoriAndOmori <skywhite15@korea.ac.kr>
2019-08-23 11:25:21 +0900

    fix mother birthday

--- app/src/main/java/com/osori/lovesj/anniversary/BirthDay.java
+++ app/src/main/java/com/osori/lovesj/anniversary/BirthDay.java
@@ -6,7 +6,7 @@ public enum BirthDay {
 	기훈(LocalDate.of(1990, 6, 23), false),
 	수진(LocalDate.of(1993, 9, 23), false),
 	아버지(LocalDate.of(1961, 9, 19), false),
-	어머니(LocalDate.of(1962, 12, 12), false),
+	어머니(LocalDate.of(1962, 12, 21), false),
 	예지(LocalDate.of(1993, 5, 20), false),
 	아버님(LocalDate.of(1961, 7, 14), true),
 	어머님(LocalDate.of(1969, 7, 5), true),
